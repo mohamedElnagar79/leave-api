c54c7c7f8c92e77ea166e065b5842d19ed697fd2
mohamedElnagaraffsquare <mohamedsaid@affsquare.com>
2024-11-28 15:50:44 +0200

    update leaves model

--- models/leaves.model.js
+++ models/leaves.model.js
@@ -17,67 +17,54 @@ const leaves = sequelize.define(
     admission_date_en: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     admission_date_ar: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     discharge_date_en: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     discharge_date_ar: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     issue_date: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     name_en: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     name_ar: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     national_id: {
       type: Sequelize.INTEGER,
       allowNull: false,
-      unique: true,
     },
     employer: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     physician_name_en: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     physician_name_ar: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     position_en: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
     position_ar: {
       type: Sequelize.STRING(191),
       allowNull: false,
-      unique: true,
     },
   },
 
